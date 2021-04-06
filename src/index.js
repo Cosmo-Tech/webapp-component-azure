e84a88ea617fbb618592e1b92732656232ba361f
jreynard <jeremy.reynard@cosmotech.com>
2021-04-06 11:18:43 +0200

    Add LICENSE and headers

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
 export { default as AuthStaticWebApp } from './AuthStaticWebApp/AuthStaticWebApp'
 export { default as  AuthMSAL } from './AuthMSAL/AuthMSAL'
