7a5c9c6f9c663fa40ab3b299781fd24fb2fcdfce
Tristan Huet <tristan.huet@cosmotech.com>
2021-06-08 09:37:38 +0200

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-export { default as AuthStaticWebApp } from './AuthStaticWebApp'
-export { default as  AuthMSAL } from './AuthMSAL'
+export { default as AuthStaticWebApp } from './AuthStaticWebApp';
+export { default as AuthMSAL } from './AuthMSAL';
