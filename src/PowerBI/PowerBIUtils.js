38331453cd15a077714dfc730cb0912897b21b42
Nicolas Borde <nicolas.borde@cosmotech.com>
2023-11-14 16:44:44 +0100

    style: update imports order in src directory

--- src/PowerBI/PowerBIUtils.js
+++ src/PowerBI/PowerBIUtils.js
@@ -1,6 +1,5 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-
 import { PowerBIReportEmbedSimpleFilter, PowerBIReportEmbedMultipleFilter } from './PowerBIReportEmbedFilter';
 import { ScenarioDTO } from './ScenarioDTO';
 
