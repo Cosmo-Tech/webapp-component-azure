5681a375658176dcf7d580bdd34724a074d924d8
Tristan Huet <tristan.huet@cosmotech.com>
2026-03-17 11:35:10 +0100

    build: remove unnecessary rollup plugins & postcss dev dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,8 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import image from '@rollup/plugin-image';
 import resolve from '@rollup/plugin-node-resolve';
 import { readFileSync } from 'fs';
 import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 const pkg = JSON.parse(readFileSync('./package.json'));
 
@@ -17,7 +14,6 @@ const config = {
   ],
   plugins: [
     external(),
-    postcss(),
     babel({
       exclude: 'node_modules/**',
       babelHelpers: 'bundled',
@@ -25,8 +21,6 @@ const config = {
     }),
     resolve(),
     commonjs({ strictRequires: 'auto' }),
-    image(),
-    visualizer(),
   ],
 };
 
